dd9803317a9e0e5ec12d88cfa4e2101e2b5e0da1
drielsz <adrielllxavi@outlook.com>
2024-10-31 09:31:39 -0300

    first-commit

---
+++ vscode-status.js
@@ -0,0 +1,26 @@
+import e from "express";
+import cors from 'cors';
+
+const app = e()
+const PORT = process.ENV || 3000;
+
+
+app.use(cors());
+app.use(e.json());
+
+
+let vscodeStatus = 0;
+
+app.post('/vscode-status', (req, res) => {
+    vscodeStatus = req.body.status;
+    console.log(`Status do vscode atualizado: ${vscodeStatus ? 'Aberto' : 'Fechado'}`);
+    res.sendStatus(200)
+});
+
+app.get('/', (req, res) => {
+    res.json({status: vscodeStatus})
+})
+
+app.listen(PORT, () => {
+    console.log(`Servidor no railway rodando na porta ${PORT}`)
+})
